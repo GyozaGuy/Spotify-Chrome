99733cf1da6b82161d492cf3ae45c0355887f1a4
Steven <gyozaguy@gmail.com>
2015-06-22 17:03:48 -0600

    Reorganized some files

--- background.js
+++ background.js
@@ -1,9 +1,6 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('index.htm', {
     id: 'embedder',
-    innerBounds: {
-      width: 1300,
-      height: 700
-    }
+    innerBounds: {width: 1300, height: 700}
   });
 });
