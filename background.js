15d8b0ef762ccae47866a3457511008d5ca9cdb6
Steven Stromberg <steven.stromberg@intel.com>
2015-09-15 08:43:15 -0600

    Changed internal app ID

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('index.htm', {
-    id: 'embedder',
+    id: 'spotify',
     innerBounds: {width: 1300, height: 700}
   });
 });
